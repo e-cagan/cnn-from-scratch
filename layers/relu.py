49e5c41ddd92d64523b003e624ca6807e3f3a826
e-cagan <emincaganapaydin@gmail.com>
2026-02-13 15:34:42 +0300

    Implemented ReLU layer.

---
+++ layers/relu.py
@@ -0,0 +1,34 @@
+"""
+Module for Rectified Linear Unit (ReLU) activation function.
+"""
+
+import numpy as np
+from base_layer import BaseLayer
+
+
+# Define a class for ReLU activation
+class ReLU(BaseLayer):
+    """
+    Class for ReLU activation function.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    # Forward and backward propagation functions for ReLU
+    def forward(self, x):
+        """
+        ReLU forward propagation function.
+        """
+
+        # Store the input on the cache to use in backward propagation
+        self.cache["x"] = x
+
+        return np.maximum(0, x)
+
+    def backward(self, dout):
+        """
+        ReLU backward propagation function.
+        """
+        
+        return dout * (self.cache["x"] > 0)
