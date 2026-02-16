f32052c69c01695de9774c819282ea94bd36d660
e-cagan <emincaganapaydin@gmail.com>
2026-02-16 19:33:12 +0300

    Updated .gitignore

---
+++ tests/test_fc.py
@@ -0,0 +1,17 @@
+"""
+Module for testing the fully connected layer gradient tests and implementation.
+"""
+
+import numpy as np
+from layers.fc import FCLayer
+from utils.gradient_check import gradient_check
+
+
+# Test out the implementation
+if __name__ == '__main__':
+    # Initializing layer and input sample
+    layer = FCLayer(in_features=8, out_features=4)
+    random_sample = np.random.sample(size=(3, 8))
+
+    # Gradient check
+    max_err = gradient_check(layer=layer, x=random_sample)
